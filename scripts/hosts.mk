9f2fbd0bd93976596e10f8aefaea39da736bad9d
huaixv <44743118+huaixv@users.noreply.github.com>
2025-02-24 17:22:04 +0800

    feat: add lab mirrors hosts

--- scripts/hosts.mk
+++ scripts/hosts.mk
@@ -1,5 +1,9 @@
 HOSTS_OPTS :=
 
+ifeq ($(HOSTS_LAB_MIRRORS),y)
+HOSTS_OPTS += --add-host mirrors.lab.loongson.cn:172.17.103.58
+endif
+
 ifneq ($(HOSTS_EXTRA),)
 HOSTS_OPTS += $(addprefix --add-host ,$(call unquote,$(HOSTS_EXTRA)))
 endif
