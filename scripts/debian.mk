f3f15540515c3f2578a3494f5730a916c9dcf80c
gengyiyang <gengyiyang@aliyun.com>
2025-04-08 20:05:14 +0800

    [feat] add Debian multi-verstion support

--- scripts/debian.mk
+++ scripts/debian.mk
@@ -2,9 +2,14 @@ ifeq ($(DISTRO_debian),y)
 
 debian_setup:
 ifeq ($(COPY_APT_SOURCES_LIST),y)
-	docker cp ./etc/apt/sources.list $(NAME):/etc/apt/sources.list
-	docker exec $(NAME) apt update
-	docker exec $(NAME) apt install -y apt-transport-https ca-certificates
+ifeq ($(TAG_bookworm),y)
+	docker cp ./etc/apt/sources.list.bookworm $(NAME):/etc/apt/sources.list
+endif
+ifeq ($(TAG_bullseye),y)
+	docker cp ./etc/apt/sources.list.bullseye $(NAME):/etc/apt/sources.list
+endif
+ifeq ($(TAG_sid),y)
+	docker cp ./etc/apt/sources.list.sid $(NAME):/etc/apt/sources.list
 endif
 
 install: debian_setup
