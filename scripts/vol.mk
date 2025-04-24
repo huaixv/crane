9d16a03d3b8951f0f11acd393f24dc1af7646934
huaixv <44743118+huaixv@users.noreply.github.com>
2025-04-24 09:56:17 +0800

    feat: change to bind mount

--- scripts/vol.mk
+++ scripts/vol.mk
@@ -1,33 +1,31 @@
 VOL_OPTS :=
 
-VOL_BIND_OPT := type=volume,volume-driver=local,volume-opt=type=none,volume-opt=o=bind
-
 ifeq ($(VOLUME_HOME),y)
 $(shell mkdir -p $(VOLUME_HOME_TO))
-VOL_OPTS += --mount $(VOL_BIND_OPT),dst=/home,volume-opt=device=$(abspath $(VOLUME_HOME_TO))
+VOL_OPTS += --mount type=bind,dst=/home,src=$(abspath $(call unquote,$(VOLUME_HOME_TO)))
 endif
 
 ifeq ($(VOLUME_ETC),y)
 $(shell mkdir -p $(VOLUME_ETC_TO))
-VOL_OPTS += --mount $(VOL_BIND_OPT),dst=/etc,volume-opt=device=$(abspath $(VOLUME_ETC_TO))
+VOL_OPTS += --mount type=bind,dst=/etc,src=$(abspath $(call unquote,$(VOLUME_ETC_TO)))
 endif
 
 ifeq ($(VOLUME_VAR),y)
 $(shell mkdir -p $(VOLUME_VAR_TO))
-VOL_OPTS += --mount $(VOL_BIND_OPT),dst=/var,volume-opt=device=$(abspath $(VOLUME_VAR_TO))
+VOL_OPTS += --mount type=bind,dst=/var,src=$(abspath $(call unquote,$(VOLUME_VAR_TO)))
 endif
 
 ifeq ($(VOLUME_SRV),y)
 $(shell mkdir -p $(VOLUME_SRV_TO))
-VOL_OPTS += --mount $(VOL_BIND_OPT),dst=/srv,volume-opt=device=$(abspath $(VOLUME_SRV_TO))
+VOL_OPTS += --mount type=bind,dst=/srv,src=$(abspath $(call unquote,$(VOLUME_SRV_TO)))
 endif
 
 ifeq ($(VOLUME_DEV),y)
-VOL_OPTS += --mount type=volume,dst=/dev,src=$(abspath $(call unquote,$(VOLUME_DEV_TO)))
+VOL_OPTS += --mount type=bind,dst=/dev,src=$(abspath $(call unquote,$(VOLUME_DEV_TO)))
 endif
 
 ifeq ($(VOLUME_SYS),y)
-VOL_OPTS += --mount type=volume,dst=/sys,src=$(abspath $(call unquote,$(VOLUME_SYS_TO)))
+VOL_OPTS += --mount type=bind,dst=/sys,src=$(abspath $(call unquote,$(VOLUME_SYS_TO)))
 endif
 
 ifneq ($(VOLUME_EXTRA),)
